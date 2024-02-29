32144600ef4d0caf7ce5712ca3bde9e0eac9c07c
YounnIm <younnusiman@gmail.com>
2024-02-29 15:05:20 -0700

    Added, add, complete and list/copy tasks need delete, edit, and undo

--- labE/edu/ucalgary/oop/ToDoList.java
+++ labE/edu/ucalgary/oop/ToDoList.java
@@ -12,4 +12,36 @@ public class ToDoList implements IToDoList {
         this.tasks = new ArrayList<>();
         this.history = new Stack<>();
     }
+
+    @Override
+    public void addTask(Task task) {
+        history.push(copyTasks(tasks));
+        tasks.add(new Task(task));
+    }
+
+    @Override
+    public void completeTask(String id) {
+        history.push(copyTasks(tasks));
+        for (Task task : tasks) {
+            if (task.getId().equals(id)) {
+                task.setCompleted(true);
+                break;
+            }
+        }
+    }
+
+// need edit, delete and undo
+
+    @Override
+    public List<Task> listTasks() {
+        return new ArrayList<>(tasks);
+    }
+
+    private List<Task> copyTasks(List<Task> tasks) {
+        List<Task> copy = new ArrayList<>();
+        for (Task task : tasks) {
+            copy.add(new Task(task));
+        }
+        return copy;
+    }
 }
