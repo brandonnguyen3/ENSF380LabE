d7dcceb761fbc9dceca43c3932b8f2c3d5af8722
brandonnguyen3 <144299768+brandonnguyen3@users.noreply.github.com>
2024-02-29 14:54:22 -0700

    IToDoList.java

---
+++ labE/edu/ucalgary/oop/IToDoList.java
@@ -0,0 +1,12 @@
+package edu.ucalgary.oop;
+
+import java.util.List;
+
+public interface IToDoList {
+    void addTask(Task task);
+    void completeTask(String id);
+    void deleteTask(String id);
+    void editTask(String id, String newTitle, boolean isCompleted);
+    void undo();
+    List<Task> listTasks();
+}
