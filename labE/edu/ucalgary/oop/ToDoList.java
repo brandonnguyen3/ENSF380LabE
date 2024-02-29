fa52d94338e2d0a87c0ddc6b27e4e46944138f16
YounnIm <younnusiman@gmail.com>
2024-02-29 14:46:07 -0700

    Possible implementation of the IToDoList interface

--- labE/edu/ucalgary/oop/ToDoList.java
+++ labE/edu/ucalgary/oop/ToDoList.java
@@ -1,1 +1,15 @@
 package edu.ucalgary.oop;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class ToDoList implements IToDoList {
+    private List<Task> tasks;
+    private Stack<List<Task>> history;
+
+    public ToDoList() {
+        this.tasks = new ArrayList<>();
+        this.history = new Stack<>();
+    }
+}
