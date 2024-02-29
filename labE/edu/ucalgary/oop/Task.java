dd3d5ba79c833d5b9e8b0730fa548f616e924c6f
brandonnguyen3 <144299768+brandonnguyen3@users.noreply.github.com>
2024-02-29 14:52:40 -0700

    Rough Task.java

--- labE/edu/ucalgary/oop/Task.java
+++ labE/edu/ucalgary/oop/Task.java
@@ -3,6 +3,42 @@ package edu.ucalgary.oop;
 import java.util.Objects;
 
 public class Task {
+    private String id;
+    private String title;
+    private boolean isCompleted;
+
+    // Constructor
+    public Task(String id, String title, boolean isCompleted) {
+        this.id = id;
+        this.title = title;
+        this.isCompleted = isCompleted;
+    }
+
+    // Copy method for deep copying task objects
+    public Task copy() {
+        return new Task(this.id, this.title, this.isCompleted);
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
 
     @Override
     public boolean equals(Object obj) {
@@ -11,7 +47,7 @@ public class Task {
         Task task = (Task) obj;
         return Objects.equals(id, task.id) &&
                Objects.equals(title, task.title) &&
-               isCompleted == task.isCompleted; 
+               isCompleted == task.isCompleted;
     }
 
     @Override
